7dca8ecc03242494e517a0faa0ebeb9a18d19326
haiyiliu314 <liuhai@umich.edu>
2017-04-18 16:22:20 -0400

    Add files via upload
    Runga-kutta almost done

--- test1.f90
+++ test1.f90
@@ -6,13 +6,17 @@ use constants
   complex*16                                       ::g1,p
 !  double precision, parameter                      ::gamma = 0.2d12
   double precision, parameter                      ::omega_r = 3.2*1.6d-19/6.63d-34
-  double precision                                 ::t
-  double precision                                 ::omegat
+  double precision                                 ::t, k1, k2, k3, k4
+  double precision                                 ::omegat1
   double precision                                 ::funcf
-  funcf(p, t) = aimag(p+t)
-  runge_kuttaf = funcf(g1, f1)
+  funcf(p) = aimag(omegat1*p)*2
+ runge_kuttaf = funcf(g1)
+  k1 = dt * funcf(g1)
+  k2 = dt * funcf(g1+k1/2)
+  k3 = dt * funcf(g1+k2/2)
+  k4 = dt * funcf(g1+k3)
 !common omegat(1:10)
-write(*,*) dt
+write(*,*) gamma
 end function runge_kuttaf
 
 
@@ -24,13 +28,12 @@ program main
 !  double precision                               ::gamma = 0.2d12
   double precision, parameter                    ::f0 = 0d0, t_end = 1d-12, &
                                                    T = 0.1d-12 , tgrid = t_end/dt
-  double precision                               ::omegat(tgrid+1,1) = sqrt(0.1)*1d12 
   double precision                               ::f(tgrid+1,1)
   complex*16                                     ::p(tgrid+1,1)
   double precision                               ::runge_kuttaf
-  common omegat
-  p0 = runge_kuttaf(1d0, cmplx(1d0,1d0), 1d0, 10)
-!  gamma = 1
-!write(*,*) omegat
 
+  gamma = 1
+  p0 = runge_kuttaf(1d0, cmplx(1d0,1d0), 1d0, 10)
+
+!write(*,*) omegat
 end program main
