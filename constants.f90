b1e967bd7eda6163c9702177f21110c33371ee28
haiyiliu314 <liuhai@umich.edu>
2017-04-20 15:12:19 -0400

    Add files via upload

--- constants.f90
+++ constants.f90
@@ -1,6 +1,6 @@
 module constants
 implicit none
 double precision                             ::gamma = 0.2d12, omegat = sqrt(0.1d0)*1d12
-double precision, parameter                  ::dt = 1d-13 
+double precision, parameter                  ::dt = 1d-15 
 
 end module constants
